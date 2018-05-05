b891d194cec3ae91ba051b0b57c4e311341c091c
Lyman Johnson <lyman.johnson@gmail.com>
2018-05-04 20:08:57 -0400

    Fixes typo in settings.py

--- clicker/settings.py
+++ clicker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'game.admin.GameConfig',
+    'game.apps.GameConfig',
 ]
 
 MIDDLEWARE = [
