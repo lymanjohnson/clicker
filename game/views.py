a2219109f89d6617cda813e5fb55253ca7c3e40a
Lyman Johnson <lyman.johnson@gmail.com>
2018-05-04 20:11:40 -0400

    Adds placeholder class based views, with 'pass' for all code

--- game/views.py
+++ game/views.py
@@ -1,3 +1,27 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from game.models import Game, Player
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import render
 
-# Create your views here.
+class WelcomeScreen(TemplateView):
+    pass
+
+class PlayerList(ListView):
+    pass
+
+class PlayerRegister(FormView):
+    pass
+
+class PlayerLogin(TemplateView):
+    pass
+
+class PlayerDetail(DetailView):
+    pass
+
+class PlayerGameList(ListView):
+    pass
+
+class GamePlay(TemplateView):
+    pass
