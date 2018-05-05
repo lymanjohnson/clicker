1e0860bbbc513e01c3d43bd031e7dd3c8dbbd9f5
Lyman Johnson <lyman.johnson@gmail.com>
2018-05-04 20:01:17 -0400

    Adds basic Game and Player models, with Player has_many Games

--- game/models.py
+++ game/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse, reverse_lazy
 
-# Create your models here.
+class Player(models.Model):
+    handle = models.CharField(max_length=30)
+    email = models.EmailField()
+    games = models.ManyToManyField('Game')
+
+    def __str__(self):
+        return self.handle
+
+class Game(models.Model):
+    score = models.IntegerField(default=0)
+    owner = models.ForeignKey('Player', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return "%s's Game #%s, %s points" % self.owner.handle, self.pk, self.score
